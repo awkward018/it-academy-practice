7e8dd4053fd0900e07f8e2a82c0c2005d211c068
саня высочина <2001sashka@tut.by>
2019-03-21 20:09:09 +0300

    commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] array = new object[3];
+            array[0] = 32;
+            array[1] = 'A';
+            string s1 = "Hello";
+            array[2] = s1;
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+
+
+            Console.ReadLine();
+        
+    }
+    }
+}
