0b74d859cd45fae87a76c4281c4d3a292cc7eaeb
саня высочина <2001sashka@tut.by>
2019-03-28 19:58:18 +0300

    stringbuilder

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello, Guys!");
+            Console.WriteLine(sb.Capacity);
+            Console.WriteLine(sb.Length);
+            Console.WriteLine(sb.MaxCapacity);
+
+            sb.Append("\nHello");
+            Console.WriteLine($"{sb.Length}---{sb.Capacity}");
+
+      
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
