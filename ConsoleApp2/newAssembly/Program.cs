4ececa700fc828a1e4de3361f3fe0203129ceccb
саня высочина <2001sashka@tut.by>
2019-03-28 21:38:41 +0300

    class

--- ConsoleApp2/newAssembly/Program.cs
+++ ConsoleApp2/newAssembly/Program.cs
@@ -1,26 +1,17 @@
-﻿using System;
+﻿using ConsoleApp2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace ConsoleApp2
+namespace newAssembly
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Motorcycle motorcycle;
-            motorcycle = new Motorcycle();
-
-
-            Console.ReadLine();
+            Motorcycle moto = new Motorcycle();
         }
-    }
-
-    public class Motorcycle
-    {
-
     }
 }
