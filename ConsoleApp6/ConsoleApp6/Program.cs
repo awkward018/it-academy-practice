cb18d713a448ca52a9ae7154a0ff71c375e9aa89
саня высочина <2001sashka@tut.by>
2019-04-09 21:41:40 +0300

    annonymous type

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var moto = new
+            {
+                Name = "Honda",
+                Year = 2004,
+                Odometer = 35000
+            };
+
+            Console.WriteLine(moto);
+            Console.WriteLine($"N - {moto.Name} {moto.Odometer} {moto.Year}");
+            Console.WriteLine(moto.GetType());
+
+            Console.ReadLine();
+        }
+    }
+}
