0a05c0b747001bf564a73faf81d5146e12356dc4
саня высочина <2001sashka@tut.by>
2019-04-09 20:09:09 +0300

    struct

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle moto = new Motorcycle();
+            //Motorcycle moto;
+            moto.Name = "Honda";
+            var type = moto.GetType();
+
+
+            Console.WriteLine(moto.GetHashCode());
+            Console.WriteLine(moto.GetType());
+
+            Console.ReadLine();
+        }
+    }
+    struct Motorcycle
+    {
+        public string Name { get; set; }
+    }
+}
